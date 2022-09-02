10fbc6e6f068a036ed2f656994854a692bedeeb2
vrrgithub1 <106780561+vrrgithub1@users.noreply.github.com>
2022-09-02 15:44:51 -0400

    Update streamlit_app.py
    Updated Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
