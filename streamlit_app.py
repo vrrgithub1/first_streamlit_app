3c322ef1affaefd055590f7886d01b303a7e6daf
vrrgithub1 <106780561+vrrgithub1@users.noreply.github.com>
2022-09-03 10:32:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ except URLError as e:
    streamlit.error()
 
 
-# my_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor()
 # my_cur.execute("select * from fruit_load_list")
 # my_data_rows = my_cur.fetchall()
 
